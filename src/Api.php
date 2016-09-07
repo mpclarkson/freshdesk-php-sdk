f2ccc08a473441bf8b9c287a5cf9717533a14a01
Giel Wijgergangs <giel@cyclesoftware.nl>
2016-09-07 14:04:10 +0200

    Fix client config params

--- src/Api.php
+++ src/Api.php
@@ -192,9 +192,7 @@ class Api
         $this->baseUrl = sprintf('https://%s.freshdesk.com/api/v2', $domain);
 
         $this->client = new Client([
-                'defaults' => [
-                    'auth' => [$apiKey, 'X']
-                ]
+                'auth' => [$apiKey, 'X']
             ]
         );
 
